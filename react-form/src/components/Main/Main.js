2558546a09e38149c2c6322151bfeaa009e931c5
vituuushka <usmanova.vita13@gmail.com>
2023-12-13 14:00:30 +0400

    main val

--- react-form/src/components/Main/Main.js
+++ react-form/src/components/Main/Main.js
@@ -2,9 +2,19 @@ import React from "react";
 import './Main.css'
 import avatar from '../../assets/images/avatar.png'
 import { useForm } from "react-hook-form";
+import * as yup from 'yup'
+import {yupResolver} from '@hookform/resolvers/yup'
+import folder from '../../assets/images/folder.png'
 
+const schema = yup.object().shape({
+    phone: yup.string().required().matches(/[+7][0-9]{10}$/, 'Неверный номер телефона'),
+    email: yup.string().required().email()
+})
 const Main = () => {
-    const { register, handleSubmit, errors } = useForm({})
+    const { register, handleSubmit, formState:{errors} } = useForm({
+        mode: "onBlur",
+        resolver: yupResolver(schema)
+    })
     const onSubmit = (data) => {
         console.log(data)
     }
@@ -18,15 +28,15 @@ const Main = () => {
                     <div className="header-title">Алексей Иванов</div>
                     <div className="header-items">
                         <div className="header-item">
-                            <img src="" />
+                            <img src={folder} />
                             <div className="header-item-text">Telegram</div>
                         </div>
                         <div className="header-item">
-                            <img src="" />
+                            <img src={folder} />
                             <div className="header-item-text">GitHub</div>
                         </div>
                         <div className="header-item">
-                            <img src="" />
+                            <img src={folder} />
                             <div className="header-item-text">Резюме</div>
                         </div>
                     </div>
@@ -35,11 +45,15 @@ const Main = () => {
             <form onSubmit={handleSubmit(onSubmit)} className="main-form">
                 <div className="main-input">
                     <label>Номер телефона</label>
-                    <input type="number" name="phone" placeholder="+7 999 999-99-99" />
+                    <input type="number" name="phone" 
+                    placeholder="+7 999 999-99-99" {...register("phone")}/>
+                    <p>{errors.phone?.message}</p>
                 </div>
                 <div className="main-input">
                     <label>Email</label>
-                    <input type="email" name="email" placeholder="webstudio.fractal@example.com" />
+                    <input type="email" name="email" 
+                    placeholder="webstudio.fractal@example.com" {...register("email")} />
+                    <p>{errors.email?.message}</p>
                 </div>
                 <button type="submit" className="button-start">Начать</button>
             </form>
