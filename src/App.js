a9593811af52a1dc790b78a073bcf834bb3999ff
vituuushka <usmanova.vita13@gmail.com>
2023-12-15 02:17:28 +0400

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Main from "./components/Main/Main";
 import Step1 from "./components/Step1/Step1";
@@ -9,14 +9,14 @@ import Step3 from "./components/Step3/Step3";
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route exact path="/" element={<Main />} />
           <Route path="/step1" element={<Step1 />} />
           <Route path="/step2" element={<Step2 />} />
           <Route path="/step3" element={<Step3 />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
